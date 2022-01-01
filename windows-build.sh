0562260a98c589e5dff9b94178926dd7e8bc4cba
dogerish <luca.filmstrip@gmail.com>
2022-01-01 12:25:22 -0500

    Add font to windows build

--- windows-build.sh
+++ windows-build.sh
@@ -2,4 +2,4 @@
 # builds a zip of the project for windows
 make CROSS=WIN32
 rm game-win32.zip 2> /dev/null
-zip dlls.zip -u game.exe textures/*.bmp maps/* -O game-win32.zip
+zip dlls.zip -u game.exe Sans.ttf textures/*.bmp maps/* -O game-win32.zip
