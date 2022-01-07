29e78a66f77610d36a654a2826150d7e056a2842
dogerish <luca.filmstrip@gmail.com>
2022-01-07 13:32:23 -0500

    Don't keep building after failed make

--- windows-build.sh
+++ windows-build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # builds a zip of the project for windows
-make CROSS=WIN32
+make CROSS=WIN32 || exit $?
 rm game-win32.zip 2> /dev/null
 zip dlls.zip -u game.exe *.ttf textures/*.bmp maps/* -O game-win32.zip
