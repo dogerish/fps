90f8cfecf415a541249c301ab5eb2012f421a3a3
dogerish <luca.filmstrip@gmail.com>
2022-01-16 13:31:02 -0500

    Better windows building

--- windows-build.sh
+++ windows-build.sh
@@ -2,12 +2,11 @@
 # builds a zip of the project for windows
 if [[ -z "$1" ]];
 then
-	"$0" x86_64
-	"$0" i686
-	exit
+	"$0" x86_64 && "$0" i686
+	exit $?
 fi
 echo "---- $1"
 export CROSS=$1
 make || exit $?
 rm game-${CROSS}.zip 2> /dev/null
-zip dlls-${CROSS}.zip -u game-${CROSS}.exe *.ttf textures/*.bmp maps/* -O game-${CROSS}.zip
+zip dlls-${CROSS}.zip -u game-${CROSS}.exe font.ttf textures/[^gentex]* maps/* -O game-${CROSS}.zip
